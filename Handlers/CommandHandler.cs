8a207827db778c655ef058c55336b5b984e16668
Song <sych7389@dgu.ac.kr>
2022-08-14 04:38:24 +0900

    Initialize Commands

---
+++ Handlers/CommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System.Reflection;
+
+namespace KrTTSBot.Handlers
+{
+    public class CommandHandler
+    {
+        private static CommandService _commandService = ServiceHandler.GetService<CommandService>();
+
+        public static async Task LoadCommandAsync()
+        {
+            await _commandService.AddModulesAsync(Assembly.GetEntryAssembly(), ServiceHandler.ServiceProvider);
+            foreach (var command in _commandService.Commands)
+                Console.WriteLine($"Command {command.Name} loaded.");
+        }
+    }
+}
