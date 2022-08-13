b3b6e8820dbe60a0783d4b5120099fe2e93ea03b
Song <sych7389@dgu.ac.kr>
2022-08-14 04:40:08 +0900

    ServiceProvider

---
+++ Handlers/ServiceHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace KrTTSBot.Handlers
+{
+    public class ServiceHandler
+    {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        public static void SetProvider(ServiceCollection collection)
+            => ServiceProvider = collection.BuildServiceProvider();
+
+        public static T GetService<T>() where T : new()
+            => ServiceProvider.GetRequiredService<T>();
+    }
+}
