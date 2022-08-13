e7d56c611967b52c6025a0643e424538de4c2715
Song <sych7389@dgu.ac.kr>
2022-08-14 04:19:24 +0900

    Commands for TTS and voice channel

---
+++ Commands/TTSCommand.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace KrTTSBot.Commands
+{
+    public class TTSCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("join")]
+        public async Task JoinCommand()
+            => await Handlers.AudioHandler.JoinAsync(Context.Guild, Context.User as IVoiceState,
+                Context.Channel as ITextChannel);
+
+        [Command("leave")]
+        public async Task LeaveCommand()
+            => await Handlers.AudioHandler.LeaveAsync(Context.Guild);
+
+        [Command("tts")]
+        public async Task PlayCommand([Remainder] string text)
+        {
+            Console.WriteLine("잉?");
+            await Handlers.AudioHandler.PlayAsync(Context.User as SocketGuildUser, Context.Guild, Context.Channel as ITextChannel, text);
+        }
+
+        [Command("kr")]
+        public async Task KrCommand()
+        {
+            Console.WriteLine("잉?");
+            await Handlers.AudioHandler.PlayAsync(Context.User as SocketGuildUser, Context.Guild, Context.Channel as ITextChannel, Context.Message.ToString());
+        }
+
+    }
+}
