342c41092b6ce3b88c236a4e9f82b34502f3ad85
Song <sych7389@dgu.ac.kr>
2022-09-20 21:55:14 +0900

    시간측정을 위한 길드별 타이머 및 타이머 처리기능

---
+++ Handlers/ConnectionTimerHandler.cs
@@ -0,0 +1,36 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Victoria;
+
+namespace KrTTSBot.Handlers
+{
+    public class ConnectionTimerHandler
+    {
+        public static Dictionary<ulong, System.Timers.Timer> guildConnecionTimer = new Dictionary<ulong, System.Timers.Timer>();
+
+        public static void StartTimer(ulong guildId)
+        {
+            if(guildConnecionTimer.TryGetValue(guildId, out var timer))
+            {
+                timer.Start();
+            }
+        }
+
+        public static void StopTimer(ulong guildId)
+        {
+            if (guildConnecionTimer.TryGetValue(guildId, out var timer))
+            {
+                timer.Stop();
+                timer.Dispose();
+            }
+        }
+
+    }
+}
