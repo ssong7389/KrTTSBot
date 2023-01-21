516c3a543b09a01be1c287a3303e94d0c0b37136
ssong7389 <sych7389@dgu.ac.kr>
2023-01-21 23:19:42 +0900

    이동 커맨드  추가

--- Commands/TextCommand.cs
+++ Commands/TextCommand.cs
@@ -12,5 +12,12 @@ namespace KrTTSBot.Commands
         {
             await Handlers.CommandHandler.HelpCommandAsync(Context.Guild, Context.Channel as ITextChannel);
         }
+        [Command("move")]
+        [Alias("이동")]
+        [Remarks("봇을 다른 채널에서 사용중일 때 사용자가 접속중인 음성 채널로 옮겨올 수 있어요.")]
+        public async Task MoveCommand()
+        {
+            await Handlers.AudioHandler.ChangeCannelAsync(Context.Guild, Context.User as IVoiceState, Context.Channel as ITextChannel);
+        }
     }
 }
