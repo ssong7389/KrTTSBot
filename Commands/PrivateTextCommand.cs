097862671d0d4db4cb9d26f1ab4202ed7ea07b0a
ssong7389 <sych7389@dgu.ac.kr>
2023-01-21 23:26:43 +0900

    개인 커맨드 및 설명에서 제외할 커맨드

---
+++ Commands/PrivateTextCommand.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using Discord.Commands;
+
+
+namespace KrTTSBot.Commands
+{
+    public class PrivateTextCommand : ModuleBase<SocketCommandContext>
+    {
+        public static List<string> CommandToHide = new List<string>();
+
+        // 개인적으로 사용할 Commands
+
+    }
+}
